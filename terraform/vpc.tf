893deebc0da318bc5a239329d6e08a44bc529212
Alban Barbosa <albanbarbosa@hotmail.com>
2024-02-02 18:30:27 -0300

    naming-convention

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,23 +1,23 @@
 resource "google_compute_network" "network" {
-  name                    = "BCWResearch-Network"
+  name                    = "bcwresearch-network"
   routing_mode            = "REGIONAL"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "subnetwork" {
-  name                     = "BCWResearch-Subnetwork"
-  ip_cidr_range            = "10.0.0.0/16"  # Main CIDR range for the subnet
+  name                     = "bcwresearch-subnetwork"
+  ip_cidr_range            = "10.0.0.0/16"
   network                  = google_compute_network.network.self_link
   private_ip_google_access = true
   region                   = "southamerica-east1"
 
   secondary_ip_range {
-    range_name    = "BCWResearch-SecondaryRange1"
-    ip_cidr_range = "10.1.0.0/20"  # Secondary CIDR range for Pods
+    range_name    = "bcwresearch-secondaryrange1"
+    ip_cidr_range = "10.1.0.0/20"
   }
 
   secondary_ip_range {
-    range_name    = "BCWResearch-SecondaryRange2"
-    ip_cidr_range = "10.2.0.0/20"  # Secondary CIDR range for Services
+    range_name    = "bcwresearch-secondaryrange2"
+    ip_cidr_range = "10.2.0.0/20"
   }
 }
