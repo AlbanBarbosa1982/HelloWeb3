3da657217d7e7dcedc4766bdaff9c14025510631
Alban Barbosa <albanbarbosa@hotmail.com>
2024-02-02 15:56:55 -0300

    provider error

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,13 +13,16 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.2"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.2"
+    }
   }
 
   backend "gcs" {
     bucket = "lilteddybucketwhite"
   }
 }
-
 
 provider "google" {
   project = local.project_id
