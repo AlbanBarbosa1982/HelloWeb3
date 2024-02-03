f0af8f2612c37f62e938ad030b2cde614b525526
Alban Barbosa <albanbarbosa@hotmail.com>
2024-02-03 07:08:31 -0300

    no ssd:(

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -25,7 +25,8 @@ resource "google_container_node_pool" "app_pool" {
   node_config {
     preemptible  = false
     machine_type = "n1-standard-1"
-    disk_size_gb = 66
+    disk_size_gb = 100
+    disk_type    = "pd-standard"
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/compute",
