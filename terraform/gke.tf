5bc1fac2c36a03f5f4aec5e9a5d8b33cc912d3d2
Alban Barbosa <albanbarbosa@hotmail.com>
2024-02-03 06:44:56 -0300

    duplicates removed

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,37 +1,5 @@
-locals {
-  project_id = "dotted-lens-412717"
-  region     = "southamerica-east1"
-  default_labels = {
-    managed-by = "terraform"
-  }
-}
-
 terraform {
   required_version = "~> 1.5"
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 5.2"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 5.2"
-    }
-  }
-
-  backend "gcs" {
-    bucket = "lilteddybucketwhite"
-  }
-}
-
-provider "google" {
-  project = local.project_id
-  region  = local.region
-}
-
-provider "google-beta" {
-  project = local.project_id
-  region  = local.region
 }
 
 resource "google_container_cluster" "helloweb3" {
